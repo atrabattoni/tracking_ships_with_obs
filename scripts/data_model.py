6a2e6e0a8d9898466cb01f712ae9d71caec3a27c
Alister Trabattoni <alister.trabattoni@gmail.com>
2020-11-13 12:09:12 +0100

    Improve data_model.

--- scripts/data_model.py
+++ scripts/data_model.py
@@ -3,21 +3,18 @@ import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 
+# Parameters
+N = 3
+sigma = 0.05
+
+# Load data
 mu = xr.open_dataarray(
     "/Users/alister/Desktop/PhD/thesis/src/warp/mu.nc")
 r = np.loadtxt("/Users/alister/Desktop/PhD/thesis/src/ray-tracing/data/r.csv")
 tau = np.loadtxt(
     "/Users/alister/Desktop/PhD/thesis/src/ray-tracing/data/tau.csv", delimiter=",")
 
-dtau_1 = tau[:, 1:] - tau[:, :-1]
-dtau_2 = tau[:, 2:] - tau[:, :-2]
-dtau_3 = tau[:, 3:] - tau[:, :-3]
-
-# %% Parameters
-N = 3
-sigma = 0.05
-
-# %% Load TDOA
+# Compute TDOA
 toa = xr.DataArray(
     data=tau[:, :N+1],
     coords={
@@ -28,8 +25,8 @@ toa = xr.DataArray(
 )
 tdoa = toa.diff("interference")
 
-# %% Load amplitude data
 
+# Utils 
 
 def make_mask(sigma, tdoa, q):
     return np.exp(-((q - tdoa) / sigma)**2)
@@ -39,6 +36,7 @@ def amplitude_model(x, model):
     k, a, loc, scale = model
     return k * skewnorm.pdf(x, a, loc, scale)
 
+# Amplitude model
 
 models = {
     1: (8.9e2, 4.0, 8.0e3, 7.5e3),
@@ -52,17 +50,20 @@ amplitude = xr.DataArray(
     coords={"distance": mu["distance"], "interference": [1, 2, 3]},
     dims=("distance", "interference")
 )
-
 mask = make_mask(sigma, tdoa, mu["quefrency"])
 output = mask * amplitude
 output = output.sum("interference")
 
+# Limit to 50 km
+tdoa.loc[{"distance": slice(50000, None)}] = np.nan
+output.loc[{"distance": slice(50000, None)}] = 0
 
+# Plot
 plt.style.use("figures.mplstyle")
 fig, axes = plt.subplots(
     nrows=2, sharex=True, sharey=True, figsize=(3.4, 3.4), gridspec_kw=dict(
-        hspace=0.05, wspace=0.0,
-        left=0.10, right=1.02,
+        hspace=0.06, wspace=0.0,
+        left=0.09, right=1.03,
         bottom=0.10, top=0.98,
     ))
 
@@ -76,25 +77,18 @@ img = ax.pcolormesh(
     vmax=0.1,
 )
 fig.colorbar(img, ax=ax, pad=0.02)
-
-# ax.plot(r / 1000, dtau_1, color="C1", alpha=0.4, ls="--")
-# ax.plot(r / 1000, dtau_2, color="C3", alpha=0.4, ls="--")
-# ax.plot(r / 1000, dtau_3, color="C6", alpha=0.4, ls="--")
-# ax.plot(np.nan, np.nan, color="C1", label="2")
-# ax.plot(np.nan, np.nan, color="C3", label="4")
-# ax.plot(np.nan, np.nan, color="C6", label="6")
-# ax.legend(title="Path difference", loc="upper left")
-
-ax.set_xlim(0, 70)
-ax.set_ylim(0, 10.24)
-ax.set_ylabel("Quefrency [s]", labelpad=0)
+ax.plot(tdoa["distance"] / 1000, tdoa, color="C3", alpha=0.5, ls="--")
+ax.set_ylabel("Quefrency [s]", labelpad=2)
 
 ax = axes[1]
 img = ax.pcolormesh(mu["distance"]/1000, mu["quefrency"],
                     output, vmin=0, vmax=0.1, rasterized=True)
 fig.colorbar(img, ax=ax, pad=0.02)
 ax.set_xlabel("Distance [km]", labelpad=2)
-ax.set_ylabel("Quefrency [s]", labelpad=0)
-ax.set_xlim(0, 70)
+ax.set_ylabel("Quefrency [s]", labelpad=2)
 
-fig.savefig("figs/data_model_a.pdf")
+ax.set_xlim(0, 60)
+ax.set_ylim(0, 7)
+ax.set_yticks(np.arange(8))
+
+fig.savefig("figs/data_model.pdf")
