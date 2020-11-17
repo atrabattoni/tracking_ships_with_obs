9159d1105d5fac96246e0bf0b1e5228d53bf34aa
Alister Trabattoni <alister.trabattoni@gmail.com>
2020-11-17 15:05:53 +0100

    Add error measurements. Minimal code complete.

---
+++ scripts/error.py
@@ -0,0 +1,100 @@
+# %% Import libs
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import obsea
+
+amax = 45
+rmax = 5000
+vmax = 3
+
+# %% Prepare data
+
+# Load peaks
+a = xr.open_dataarray("a.nc")
+r = xr.open_dataarray("r.nc")
+v = xr.open_dataarray("v.nc")
+
+# Load tracks
+tracks = pd.read_pickle("tracks.pkl")
+tracks = tracks["linestring"]
+tracks = tracks.apply(obsea.track2xarr)
+tracks = tracks.apply(lambda xarr: xarr.interp_like(a))
+tracks = xr.Dataset(tracks.to_dict())
+name_dict = {key: str(k) for k, key in enumerate(tracks.keys())}
+tracks = tracks.rename(name_dict)
+obs_orientation = 77.0
+tracks *= np.exp(1j*np.deg2rad(obs_orientation))
+
+# Convert tracks into srange and speed and aling everybody
+atrack = np.rad2deg(np.arctan2(tracks.real, tracks.imag)) % 360
+rtrack = np.abs(tracks) 
+vtrack = np.abs(tracks).diff("time") / 60.0
+
+a, r, v, atrack, rtrack, vtrack = xr.align(a, r, v, atrack, rtrack, vtrack)
+
+# %% Global Error
+
+# Error
+diff_ag = (a - atrack).to_array("track")
+diff_ag = (diff_ag + 180) % 360 - 180
+diff_rg = (r - rtrack).to_array("track")
+diff_vg = (v - vtrack).to_array("track")
+
+# Remove times without data
+diff_ag = diff_ag.dropna("time", "all")
+diff_rg = diff_rg.dropna("time", "all")
+diff_vg = diff_vg.dropna("time", "all")
+
+# Chose minimum error
+index = np.abs(diff_ag).argmin("track")
+diff_ag = diff_ag[index]
+index = np.abs(diff_rg).argmin("track")
+diff_rg = diff_rg[index]
+index = np.abs(diff_vg).argmin("track")
+diff_vg = diff_vg[index]
+
+# Filter out too big errors
+cond_ag = (np.abs(diff_ag) < amax)
+diff_truncated_ag = xr.where(cond_ag, diff_ag, np.nan)
+cond_g = (np.abs(diff_rg) < rmax) & (np.abs(diff_vg) < vmax)
+diff_truncated_rg = xr.where(cond_g, diff_rg, np.nan)
+diff_truncated_vg = xr.where(cond_g, diff_vg, np.nan)
+
+# Compute SMAD
+smad_ag = 1.4826 * np.abs(diff_truncated_ag).median()
+smad_rg = 1.4826 * np.abs(diff_truncated_rg).median()
+smad_vg = 1.4826 * np.abs(diff_truncated_vg).median()
+
+# %% Per ship error
+
+# Error
+diff_a = (a - atrack)
+diff_a = (diff_a + 180) % 360 - 180
+diff_r = (r - rtrack)
+diff_v = (v - vtrack)
+
+# Filter out to big errors
+cond_a = (np.abs(diff_a) < amax)
+diff_truncated_a = xr.where(cond_a, diff_a, np.nan)
+cond_rv = (np.abs(diff_r) < rmax) & (np.abs(diff_v) < vmax)
+diff_truncated_r = xr.where(cond_rv, diff_r, np.nan)
+diff_truncated_v = xr.where(cond_rv, diff_v, np.nan)
+
+# Compute SMAD
+smad_a = 1.4826 * np.abs(diff_truncated_a).median("time")
+smad_r = 1.4826 * np.abs(diff_truncated_r).median("time")
+smad_v = 1.4826 * np.abs(diff_truncated_v).median("time")
+
+# Count values
+count_a = cond_a.apply(np.count_nonzero)
+count_rv = cond_rv.apply(np.count_nonzero)
+
+# Save to disk
+# smad_a.to_netcdf("smad_azimuth.nc")
+# smad_r.to_netcdf("smad_range.nc")
+# smad_v.to_netcdf("smad_speed.nc")
+
---
+++ scripts/tracking_error.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+tracks = pd.read_pickle("tracks.pkl")
+lines = pd.read_pickle("lines.pkl")
+
+lines["cpa_distance"] = np.abs(lines["cpa_distance"])
+lines["speed_heading"] = (np.rad2deg(lines["speed_heading"]) + 77) % 360
+
+# Ship Information
+
+type2name = {
+    70: "Cargo",
+    71: "Cargo",
+    79: "Cargo",
+    80: "Tanker",
+}
+
+shipInfo = tracks[["mmsi", "name", "type", "length", "width", "draught"]]
+shipInfo.columns = shipInfo.columns.str.capitalize()
+shipInfo["Name"] = shipInfo["Name"].str.title()
+shipInfo["Type"] = shipInfo["Type"].apply(lambda x: type2name[x])
+shipInfo.to_csv("shipInfo.csv", sep=";")
+
+error = tracks[["cpa_time", "cpa_distance", "speed_value", "speed_heading"]]
+error["cpa_time"] = error["cpa_time"].astype(int) / 1e9
+error = np.abs(lines - error)
+error["speed_heading"] = error["speed_heading"] % 360
+error["speed_value"] = error["speed_value"] * 1.94384
+error
+
+
+error["speed_heading"].median() * 1.4826
+error["cpa_time"].median() * 1.4826
+error["speed_value"].iloc[:10].median() * 1.4826
+error["cpa_distance"].iloc[:10].median() * 1.4826
