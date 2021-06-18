7de27a1369d4860c7d4acf5887937cbb52ba93e5
Alister Trabattoni <alister.trabattoni@gmail.com>
2021-06-18 11:47:52 +0200

    Fix things.

--- 4_tracking/3_error.py
+++ 4_tracking/3_error.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 
-tracks = pd.read_pickle("../data/tracks.pkl")
+tracks = pd.read_pickle("../inputs/tracks.pkl")
 lines = pd.read_pickle("../data/lines.pkl")
+
+tracks = tracks.set_index(np.arange(1, len(tracks) + 1))
 
 lines["cpa_distance"] = np.abs(lines["cpa_distance"])
 lines["speed_heading"] = (np.rad2deg(lines["speed_heading"]) + 77) % 360
@@ -16,11 +18,11 @@ type2name = {
     80: "Tanker",
 }
 
-shipInfo = tracks[["mmsi", "name", "type", "length", "width", "draught"]]
-shipInfo.columns = shipInfo.columns.str.capitalize()
-shipInfo["Name"] = shipInfo["Name"].str.title()
-shipInfo["Type"] = shipInfo["Type"].apply(lambda x: type2name[x])
-shipInfo.to_csv("shipInfo.csv", sep=";")
+# shipInfo = tracks[["mmsi", "name", "type", "length", "width", "draught"]]
+# shipInfo.columns = shipInfo.columns.str.capitalize()
+# shipInfo["Name"] = shipInfo["Name"].str.title()
+# shipInfo["Type"] = shipInfo["Type"].apply(lambda x: type2name[x])
+# shipInfo.to_csv("shipInfo.csv", sep=";")
 
 error = tracks[["cpa_time", "cpa_distance", "speed_value", "speed_heading"]]
 error["cpa_time"] = error["cpa_time"].astype(int) / 1e9
@@ -28,7 +30,6 @@ error = np.abs(lines - error)
 error["speed_heading"] = error["speed_heading"] % 360
 error["speed_value"] = error["speed_value"] * 1.94384
 error
-
 
 error["speed_heading"].median() * 1.4826
 error["cpa_time"].median() * 1.4826
