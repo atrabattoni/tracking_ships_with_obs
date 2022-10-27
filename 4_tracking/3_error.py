cdf9c359d39e4dd901c417cdae3378d91202599c
Alister Trabattoni <alister.trabattoni@gmail.com>
2022-10-27 15:58:33 +0200

    Fix way of computing error

--- 4_tracking/3_error.py
+++ 4_tracking/3_error.py
@@ -11,19 +11,13 @@ measlines = pd.read_csv("../data/lines.csv", index_col="ntrack")
 records = []
 for idx, track in enumerate(tracks, start=1):
     data = {}
-    track = track.isel(time=[0, -1])
     cpa = obsea.get_cpa(track)
-    dr = (track.isel(time=-1) - track.isel(time=0)).values
-    dt = (
-        (track.isel(time=-1)["time"] - track.isel(time=0)["time"])
-        / np.timedelta64(1, "s")
-    ).values
-    speed = dr / dt
+    time = cpa["time"].values[0]
+    cpa = cpa.values[0]
+    speed = obsea.simplify(track)["v"].values[0]
     data["ntrack"] = idx
-    data["cpa_time"] = (cpa["time"].values[0] - np.datetime64(0, "s")) / np.timedelta64(
-        1, "s"
-    )
-    data["cpa_distance"] = np.abs(cpa).values[0]
+    data["cpa_time"] = (time - np.datetime64(0, "s")) / np.timedelta64(1, "s")
+    data["cpa_distance"] = np.abs(cpa)
     data["speed_heading"] = np.rad2deg(np.arctan2(speed.real, speed.imag)) % 360
     data["speed_value"] = np.abs(speed)
     records.append(data)
@@ -38,18 +32,18 @@ error["speed_value"] = error["speed_value"] * 1.94384
 
 df = pd.DataFrame(
     {
+        "cpa_time": [np.round(error["cpa_time"].median() * 1.4826, 0)],
         "cpa_distance": [
             np.round(error["cpa_distance"].iloc[:10].median() * 1.4826, 0)
         ],
-        "cpa_time": [np.round(error["cpa_time"].median() * 1.4826, 0)],
         "speed_heading": [np.round(error["speed_heading"].median() * 1.4826, 1)],
         "speed_value": [np.round(error["speed_value"].iloc[:10].median() * 1.4826, 2)],
     }
 )
 df.to_csv("../data/track_general_errors.csv")
 
-error["cpa_distance"] = np.round(error["cpa_distance"], 0)
 error["cpa_time"] = np.round(error["cpa_time"], 0)
+error["cpa_distance"] = np.round(error["cpa_distance"], 0)
 error["speed_heading"] = np.round(error["speed_heading"], 1)
 error["speed_value"] = np.round(error["speed_value"], 2)
 error.to_csv("../data/track_errors.csv")
